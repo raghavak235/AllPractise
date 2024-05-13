24d6b577db4bf357b00c1f9c2d8db412df5054f2
raghavarahul <raghavak235@gmail.com>
2024-05-13 18:51:45 -0400

    Updating Github Questions

--- github_practise_questions.py
+++ github_practise_questions.py
@@ -524,3 +524,61 @@ def map_filter():
 
 def numbers():
     print(list(filter(lambda x: x%2==0, range(1,21))))
+
+# Question:
+# Define a class named American which has a static method called printNationality.
+
+class American:
+    @staticmethod
+    def printNationality(country):
+        return country
+
+# Question:
+# Define a class named American and its subclass NewYorker.
+
+class American:
+    pass
+class NewYorker(American):
+    pass
+
+# Question:
+# Define a class named Circle which can be constructed by a radius. The Circle class has a method which can compute the area.
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+    def area(self):
+        return 3.14 * self.radius ** 2
+
+# Define a class named Rectangle which can be constructed by a length and width. The Rectangle class has a method which can compute the area.
+
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+
+
+# Define a class named Shape and its subclass Square. The Square class has an init function which takes a length as argument.
+# Both classes have a area function which can print the area of the shape where Shape's area is 0 by default.
+
+class Shape:
+    def __init__(self):
+        pass
+    def area(self):
+        print("Area of shape is 0")
+        return 0
+
+class Square(Shape):
+    def __init__(self, length):
+        Shape.__init__(self)
+        self.length = length
+
+
+    def area(self):
+        return self.length * self.length
+
+
